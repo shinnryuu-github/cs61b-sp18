9f91b585f35588f9981443346870cb62f8e02598
ryuusama <p.k17173@163.com>
2023-12-13 21:07:58 +0800

    project1b finished1

--- proj1b/PalindromeFinder.java
+++ proj1b/PalindromeFinder.java
@@ -1,16 +1,25 @@
 /** This class outputs all palindromes in the words file in the current directory. */
 public class PalindromeFinder {
-    /*
+
     public static void main(String[] args) {
         int minLength = 4;
-        In in = new In("../library-sp18/data/words.txt");
         Palindrome palindrome = new Palindrome();
-
-        while (!in.isEmpty()) {
-            String word = in.readString();
-            if (word.length() >= minLength && palindrome.isPalindrome(word)) {
-                System.out.println(word);
+        int max = 0, maxn = 1;
+        for (int i = 1; i <= 25; i++) {
+            CharacterComparator offbyn = new OffByN(i);
+            int sum = 0;
+            In in = new In("../library-sp18/data/words.txt");
+            while (!in.isEmpty()) {
+                String word = in.readString();
+                if (word.length() >= minLength && palindrome.isPalindrome(word, offbyn)) {
+                    sum++;
+                }
+            }
+            if (sum > max) {
+                max = sum;
+                maxn = i;
             }
         }
-    } Uncomment this class once you've written isPalindrome. */
+        System.out.println(max + " " + maxn);
+    }
 }
