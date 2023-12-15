9a008e2e77689edb0137e94763878c53a54b260b
ryuusama <p.k17173@163.com>
2023-12-15 12:52:53 +0800

    project1gold finished

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,52 +6,49 @@ public class TestArrayDequeGold {
     public void randomTest(){
         StudentArrayDeque<Integer> student = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solution = new ArrayDequeSolution<>();
-        int size = 0;
+        int count = 0, size = 0;
         String message = "";
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 1000; i++){
+            int randomnumber = StdRandom.uniform(4);
             int randomadd = StdRandom.uniform(1000);
-            Integer expected = 1, actuall = 1;
-            if (size == 0) {
-                int randomnumber = StdRandom.uniform(2);
-                if (randomnumber == 0) {
-                    solution.addFirst(randomadd);
-                    student.addFirst(randomadd);
-                    size++;
-                    message += "addFirst(" + randomadd + ")\n";
-                } else if (randomnumber == 1) {
-                    solution.addLast(randomadd);
-                    student.addLast(randomadd);
-                    size++;
-                    message += "addLast(" + randomadd + ")\n";
-                }
+            Integer expected, actuall;
+            if (randomnumber == 0){
+                solution.addFirst(randomadd);
+                student.addFirst(randomadd);
+                size++;
+                message += "addFirst(" + randomadd + ")\n";
+            }
+            else if (randomnumber == 1){
+                solution.addLast(randomadd);
+                student.addLast(randomadd);
+                size++;
+                message +="addLast(" + randomadd + ")\n";
+            }
+            else if (randomnumber == 2 && size != 0) {
+                size--;
+                expected = solution.removeFirst();
+                actuall = student.removeFirst();
+                message += "removeFirst()\n";
+                assertEquals(message, expected, actuall);
+            }
+            else if (randomnumber == 2 && size == 0) {
+                solution.addFirst(randomadd);
+                student.addFirst(randomadd);
+                size++;
+                message += "addFirst(" + randomadd + ")\n";
+            }
+            else if (randomnumber == 3 && size != 0) {
+                size--;
+                expected = solution.removeLast();
+                actuall = student.removeLast();
+                message += "removeLast()\n";
+                assertEquals(message, expected, actuall);
             }
             else {
-                int randomnumber = StdRandom.uniform(4);
-                if (randomnumber == 0) {
-                    size--;
-                    expected = solution.removeFirst();
-                    actuall = student.removeFirst();
-                    message += "removeFirst()\n";
-                }
-                else if (randomnumber == 1) {
-                    solution.addFirst(randomadd);
-                    student.addFirst(randomadd);
-                    size++;
-                    message += "addFirst(" + randomadd + ")\n";
-                }
-                else if (randomnumber == 2) {
-                    size--;
-                    expected = solution.removeLast();
-                    actuall = student.removeLast();
-                    message += "removeLast()\n";
-                }
-                else {
-                    solution.addLast(randomadd);
-                    student.addLast(randomadd);
-                    size++;
-                    message += "addLast(" + randomadd + ")\n";
-                }
-                assertEquals(message, expected, actuall);
+                solution.addLast(randomadd);
+                student.addLast(randomadd);
+                size++;
+                message += "addLast(" + randomadd + ")\n";
             }
         }
     }
